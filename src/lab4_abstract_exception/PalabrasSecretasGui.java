d797fbf4b2ca3082134a25e0c5befe578f663e68
GalLu707 <zasseuwu@gmail.com>
2026-02-13 10:10:56 -0600

    para capturas

---
+++ src/lab4_abstract_exception/PalabrasSecretasGui.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4_abstract_exception;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author USER
+ */
+public class PalabrasSecretasGui extends GuiBase{
+    private JPanel panelPrincipal;
+    private JButton btnAgregarPalabra, btnSalir;
+    private JLabel lbltitulo, lblNueva;
+    private JScrollPane palabrasActuales;
+    private DefaultTableModel modelo;
+}
