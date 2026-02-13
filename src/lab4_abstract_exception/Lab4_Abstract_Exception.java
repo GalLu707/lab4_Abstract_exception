a50137e40975040c14d6d4d8eb0d35a4f12d1032
GalLu707 <zasseuwu@gmail.com>
2026-02-13 08:20:57 -0600

    holi

---
+++ src/lab4_abstract_exception/Lab4_Abstract_Exception.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4_abstract_exception;
+
+
+public class Lab4_Abstract_Exception {
+
+    public static void main(String[] args) {
+        System.out.println("when haces tus momos en java");
+    }
+    
+}
